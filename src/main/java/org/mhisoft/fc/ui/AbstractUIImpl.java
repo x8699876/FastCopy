1af4f7982006b8f6e6bfc98e23fa0b291ada9750
I831964 <zhongyuan.xue@sap.com>
2018-06-30 12:42:43 -0400

    add github home link

--- src/main/java/org/mhisoft/fc/ui/AbstractUIImpl.java
+++ src/main/java/org/mhisoft/fc/ui/AbstractUIImpl.java
@@ -33,8 +33,8 @@ import org.mhisoft.fc.RunTimeProperties;
 public abstract class AbstractUIImpl implements UI {
 
 	public void printBuildAndDisclaimer() {
-		println("Fast Copy (v" +
-				version + ", build " + build + ", by Tony Xue, MHISoft)");
+		println("Fast Copy (v" +version + ", build " + build + ", by Tony Xue, MHISoft)");
+		println("(https://github.com/mhisoft/fastcopy)");
 	}
 
 	public void dumpArguments(String[] args, RunTimeProperties props) {
